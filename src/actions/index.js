1a5ec1cbc45dad23d234e3fbb395b7325066e5db
Alexander Brown <abrown@fiba.net>
2018-11-01 12:21:06 -0400

    adding date and delete reminder functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,22 @@
-import { ADD_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
 
-export const addReminder = (text) => {
+// action to add a reminder
+export const addReminder = (text, dueDate) => {
   const action = {
     type: ADD_REMINDER,
-    text
+    text,
+    dueDate
   }
-  // console.log('action in addReminder', action);
+  console.log('action in addReminder', action);
+  return action;
+}
+
+// action to delete a reminder
+export const deleteReminder = (id) =>  {
+  const action = {
+    type: DELETE_REMINDER,
+    id
+  }
+ console.log('deleting in actions/index.js', action);
   return action;
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,35 @@
-import { ADD_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
 
 const reminder = (action) => {
+  let { text, dueDate } = action;
   return {
-    text: action.text,
+    text,
+    dueDate,
     id: Math.random()
   }
+}
+
+const removeById = (state = [], id) => {
+  const reminders = state.filter( reminder => reminder.id !== id);
+  console.log('new reduced reminders', reminders);
+  return reminders;
 }
 
 const reminders = (state = [], action) => {
   let reminders = null;
 
   switch(action.type) {
+    // case to add reminder
     case ADD_REMINDER:
       reminders = [ ...state, reminder(action)];// "..state" spread and array ES6
-      // console.log('reminders as state', reminders);
+      console.log('reminders as state', reminders);
       return reminders;
+
+    // case to delete reminder
+    case DELETE_REMINDER:
+      reminders = removeById(state, action.id);
+      return reminders;
+
     default:
       return state;
   }
